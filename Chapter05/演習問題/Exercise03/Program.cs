06d8d4f4b01e84f5f9fe7850efcea99d86066bb4
KawashimaYuichiro <oibc35236@ojs.ac.jp>
2024-05-30 15:18:53 +0900

    4_6まで完了

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -8,6 +8,7 @@ namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
             var text = "Jackdaws love my big sphinx of quartz";
+            var text2 = "Jackdaws,love my,big sphinx-of_quartz";
 
             Exercise3_1(text);
             Console.WriteLine("-----");
@@ -22,6 +23,10 @@ namespace Exercise03 {
             Console.WriteLine("-----");
 
             Exercise3_5(text);
+            Console.WriteLine("-----");
+
+            Exercise3_6(text2);
+            Console.WriteLine("-----");
         }
 
         private static void Exercise3_1(string text) {
@@ -30,19 +35,19 @@ namespace Exercise03 {
         }
 
         private static void Exercise3_2(string text) {
-
             string ok = text.Replace("big", "small");
             Console.WriteLine("置き換え後の文字列: " + ok);
         }
 
         private static void Exercise3_3(string text) {
             var str = text.Split(' ').Length;
-            Console.WriteLine(str);
+            Console.WriteLine("単語数：" + str);
 
         }
 
         private static void Exercise3_4(string text) {
             var str = text.Split(' ').Where(word => word.Length <= 4);
+            Console.WriteLine("4文字以下の単語");
             foreach (var word in str) {
                 Console.WriteLine(word);
             }
@@ -55,6 +60,14 @@ namespace Exercise03 {
                 ss.Append(word).Append(' ');
             }
             Console.WriteLine(ss);
+        }
+
+        private static void Exercise3_6(string text2) {
+            var str = text2.Split(' ',',','-','_').Where(word => word.Length <= 4);
+            Console.WriteLine("4文字以下の単語");
+            foreach (var word in str) {
+                Console.WriteLine(word);
+            }
         }
     }
 }
---
+++ Chapter05/演習問題/Exercise04/Exercise04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+
+            string[] parts = line.Split(';');
+
+            string novelist = parts[0].Split('=')[1];
+            string bestWork = parts[1].Split('=')[1];
+            string birthYear = parts[2].Split('=')[1];
+
+            Console.WriteLine("作家：" + novelist);
+            Console.WriteLine("代表作：" + bestWork);
+            Console.WriteLine("誕生年：" + birthYear);
+
+        }
+
+        //できたら以下のメソッドを完成させて利用する
+        //static string ToJapanese(string key) {
+
+
+        //}
+    }
+}
