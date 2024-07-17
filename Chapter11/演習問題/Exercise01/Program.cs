b2d047edc13792db9e1052ec9a619a267a18a427
KawashimaYuichiro <oibc35236@ojs.ac.jp>
2024-07-17 15:58:06 +0900

    修正

--- Chapter11/演習問題/Exercise01/Program.cs
+++ Chapter11/演習問題/Exercise01/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace Exercise01 {
     internal class Program {
@@ -16,44 +18,54 @@ namespace Exercise01 {
             Exercise1_3(file);
             Console.WriteLine();
 
-            var newfile = "sports.xml";
+            var newfile = "kanzi.xml";
             Exercise1_4(file, newfile);
 
         }
 
         private static void Exercise1_1(string file) {
-            XDocument xdoc = XDocument.Load(file);
-            var sports = xdoc.Root.Elements("ballsport");
+            var xdoc = XDocument.Load(file);
+            var teams = xdoc.Root.Elements()
+                                .Select(x => new {
+                                    Name = (string)x.Element("name"),
+                                    Member = (int)x.Element("teammembers")
+                                });
 
-            foreach (var sport in sports) {
-                var name = sport.Element("name").Value;
-                var teamMembers = sport.Element("teammembers").Value;
-                Console.WriteLine($"{name}: {teamMembers}人");
+            foreach (var team in teams) {
+                Console.WriteLine($"{team.Name},{team.Member}人");
             }
+
         }
 
         private static void Exercise1_2(string file) {
-            XDocument xdoc = XDocument.Load(file);
-            var sports = xdoc.Root.Elements("ballsport").OrderBy(sport => (sport.Element("firstplayed").Value));
+            var xdoc = XDocument.Load(file);
+            var kanzi = xdoc.Root.Elements("ballsport")
+                                 .Select(x => new {
+                                     NameKanji = (string)x.Element("name").Attribute("kanji"),
+                                     FirstPlayed = (int)x.Element("firstplayed")
+                                 })
+                                 .OrderBy(x => x.FirstPlayed);
 
-            foreach (var sport in sports) {
-                string kanjiName = sport.Element("name").Attribute("kanji").Value;
-                Console.WriteLine(kanjiName);
+            foreach (var bs in kanzi) {
+                Console.WriteLine($"{bs.NameKanji}:{bs.FirstPlayed}");
             }
-
         }
 
         private static void Exercise1_3(string file) {
-            XDocument xdoc = XDocument.Load(file);
-            var sports = xdoc.Root.Elements("ballsport")
-                                 .OrderByDescending(sport => (sport.Element("teammembers").Value))
+            var xdoc = XDocument.Load(file);
+            var member = xdoc.Root.Elements("ballsport")
+                                 .Select(x => new {
+                                     Name = (string)x.Element("name"),
+                                     TeamMembers = (int)x.Element("teammembers")
+                                 })
+                                 .OrderByDescending(x => x.TeamMembers)
                                  .First();
-            string name = sports.Element("name").Value;
+            Console.WriteLine($"{member.Name}");
 
-            Console.WriteLine(name);
         }
 
         private static void Exercise1_4(string file, string newfile) {
+
         }
     }
 }
