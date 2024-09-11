f55fb162ece5b7e542cbb8ecdb27121d32cf5325
KawashimaYuichiro <oibc35236@ojs.ac.jp>
2024-09-11 14:52:00 +0900

    サーバー接続

--- FomApps/RssReader/Form1.Designer.cs
+++ FomApps/RssReader/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             // btGet
             // 
-            this.btGet.Location = new System.Drawing.Point(846, 18);
+            this.btGet.Location = new System.Drawing.Point(846, 19);
             this.btGet.Name = "btGet";
             this.btGet.Size = new System.Drawing.Size(75, 23);
             this.btGet.TabIndex = 1;
@@ -66,7 +66,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(65, 52);
+            this.label2.Location = new System.Drawing.Point(65, 55);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(86, 12);
             this.label2.TabIndex = 6;
