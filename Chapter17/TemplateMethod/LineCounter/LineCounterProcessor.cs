ffe993a4febfa367ba29b7ee76b1577d3100d8e7
KobayashiRaiki <166583783+35117KobayashiRaiki@users.noreply.github.com>
2024-10-09 14:57:48 +0900

    TemplateMethod

---
+++ Chapter17/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TextFileProcessor;
+
+namespace LineCounter {
+     class LineCounterProcessor :TextProcessor{
+
+        private int _conut;
+        string _text;
+        protected override void Initialize(string fname) {
+            _conut = 0;
+        }
+
+        protected override void Execute(string line) {
+            //Console.WriteLine(line.ToUpper());
+            _text = line;
+            _conut++;
+        }
+
+        protected override void Terminate() {
+            Console.WriteLine("{0} 行,", _conut);
+            Console.WriteLine(_text);
+        }
+    }
+}
