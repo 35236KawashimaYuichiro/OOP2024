54901f6cc2f7f8602f6ceab3e81663b632cd7d5d
KobayashiRaiki <166583783+35117KobayashiRaiki@users.noreply.github.com>
2024-11-07 10:32:50 +0900

    重さの単位を追加

--- WPF/SampleWeightUnitconverter/Model/GramUnit.cs
+++ WPF/SampleWeightUnitconverter/Model/GramUnit.cs
@@ -7,13 +7,18 @@ using System.Threading.Tasks;
 namespace SampleWeightUnitConverter {
     internal class MetricWeightUnit : WeightUnit {
         private static readonly List<MetricWeightUnit> units = new List<MetricWeightUnit> {
-            new MetricWeightUnit { Name = "g", Coefficient = 1 },          // グラム
-            new MetricWeightUnit { Name = "kg", Coefficient = 1000 }       // キログラム
+            new MetricWeightUnit { Name = "µg", Coefficient = 0.000001 }, // マイクログラム
+            new MetricWeightUnit { Name = "mg", Coefficient = 0.001 },    // ミリグラム
+            new MetricWeightUnit { Name = "g", Coefficient = 1 },         // グラム
+            new MetricWeightUnit { Name = "kg", Coefficient = 1000 },     // キログラム
+            new MetricWeightUnit { Name = "ct", Coefficient = 0.2 },      // カラット 
+            new MetricWeightUnit { Name = "t", Coefficient = 1000000 }    // トン 
         };
+
         public static ICollection<MetricWeightUnit> Units { get { return units; } }
 
         public double FromImperialUnit(ImperialWeightUnit unit, double value) {
-            return (value * unit.Coefficient) * 28.3495 / this.Coefficient; // オンスからグラムに変換
+            return (value * unit.Coefficient) * 28.3495 / this.Coefficient; 
         }
     }
 }
--- WPF/SampleWeightUnitconverter/Model/PoundUnit.cs
+++ WPF/SampleWeightUnitconverter/Model/PoundUnit.cs
@@ -7,13 +7,21 @@ using System.Threading.Tasks;
 namespace SampleWeightUnitConverter {
     internal class ImperialWeightUnit : WeightUnit {
         private static readonly List<ImperialWeightUnit> units = new List<ImperialWeightUnit> {
-            new ImperialWeightUnit { Name = "oz", Coefficient = 1 },       // オンス
-            new ImperialWeightUnit { Name = "lb", Coefficient = 16 }       // ポンド (1ポンド = 16オンス)
+            //new ImperialWeightUnit { Name = "gr", Coefficient = 1.0 / 7000 }, // グレーン 
+            //new ImperialWeightUnit { Name = "dr", Coefficient = 1.0 / 256 },  // ドラム 
+            new ImperialWeightUnit { Name = "oz", Coefficient = 1 },          // オンス
+            new ImperialWeightUnit { Name = "lb", Coefficient = 16 },        // ポンド 
+            new ImperialWeightUnit { Name = "st", Coefficient = 224 },       // ストーン 
+            new ImperialWeightUnit { Name = "long ton", Coefficient = 35840 }, // トン (ロングトン)
+            new ImperialWeightUnit { Name = "short ton", Coefficient = 32000 }, // トン (ショートトン)
+            new ImperialWeightUnit { Name = "cwt (long)", Coefficient = 1792 },  // ハンドレッドウェイト (ロング)
+            new ImperialWeightUnit { Name = "cwt (short)", Coefficient = 1600 }  // ハンドレッドウェイト (ショート)
         };
+
         public static ICollection<ImperialWeightUnit> Units { get { return units; } }
 
         public double FromMetricUnit(MetricWeightUnit unit, double value) {
-            return (value * unit.Coefficient) / 28.3495 / this.Coefficient; // グラムから変換
+            return (value * unit.Coefficient) / 28.3495 / this.Coefficient; 
         }
     }
 }
