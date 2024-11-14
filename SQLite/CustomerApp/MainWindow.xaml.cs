f63bc5ab15992fbfed1bc32cf6614380bdbf3ddb
KobayashiRaiki <166583783+35117KobayashiRaiki@users.noreply.github.com>
2024-11-14 10:32:34 +0900

    Readボタンから読み込み

---
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using CustomerApp.Objects;
+using SQLite;
+using System.Windows;
+using System;
+
+namespace CustomerApp {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e) {
+            var customer = new Customer() {
+                Name = NameTextBox.Text,
+                Phone = PhoneTextBox.Text,
+            };
+           
+            using (var connection = new SQLiteConnection(App.databasePass)) {
+                connection.CreateTable<Customer>();
+                connection.Insert(customer);
+            }
+        }
+
+        private void ReadButton_Click(object sender, RoutedEventArgs e) {
+
+            using (var connection = new SQLiteConnection(App.databasePass)) {
+                connection.CreateTable<Customer>();
+                var customers = connection.Table<Customer>().ToList();
+
+                CustomerListView.ItemsSource = customers;
+            }
+        }
+    }
+}
